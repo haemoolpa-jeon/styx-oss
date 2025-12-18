8d4e364201c4dc2aa1d063b3141f67fee1e19489
haemoolpa-jeon <haemoolpa-jeon@users.noreply.github.com>
2025-12-18 10:59:40 +0900

    config: Tauri 앱 서버 URL을 Lightsail로 변경

--- styx-desktop/client/config.js
+++ styx-desktop/client/config.js
@@ -1,8 +1,4 @@
 // Styx Desktop 설정
 // 서버 주소를 여기서 변경하세요
 
-window.STYX_SERVER_URL = 'http://localhost:3000';
-
-// 예시:
-// window.STYX_SERVER_URL = 'http://192.168.1.100:3000';
-// window.STYX_SERVER_URL = 'https://your-server.com';
+window.STYX_SERVER_URL = 'http://3.39.223.2:3000';
